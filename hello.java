e380f5c03d7eb916fbf4d143bbd8445da3ed1f3e
Haleema Sadia <95963370+Data-Ninjaa@users.noreply.github.com>
2025-05-02 15:24:00 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,6 @@
-system.out.println("hello I AM RUNNING JENKINS USING DOCKER AGENT");
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
